0b8d6d356d3906a2b0bae7f6cf6f5bc4a32190ce
younippon <hikonyan.mh36@gmail.com>
2016-07-30 19:03:58 +0900

    First commit

---
+++ app/src/androidTest/java/younippon/github/io/android_material_components/ApplicationTest.java
@@ -0,0 +1,13 @@
+package younippon.github.io.android_material_components;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/younippon/github/io/android_material_components/MainActivity.kt
@@ -0,0 +1,12 @@
+package younippon.github.io.android_material_components
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
