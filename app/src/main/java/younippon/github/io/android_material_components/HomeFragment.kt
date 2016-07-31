eedd43d3cab43ca40d5db2c85b95cd2b6b84470f
younippon <hikonyan.mh36@gmail.com>
2016-07-31 15:17:09 +0900

    Added colorand fragment

---
+++ app/src/main/java/younippon/github/io/android_material_components/HomeFragment.kt
@@ -0,0 +1,24 @@
+package younippon.github.io.android_material_components
+
+import android.app.Activity
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by yo on 16/07/31.
+ */
+
+class HomeFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val view = inflater!!.inflate(R.layout.fragment_home, container, false)
+        return view
+    }
+}
--- app/src/main/java/younippon/github/io/android_material_components/MainActivity.kt
+++ app/src/main/java/younippon/github/io/android_material_components/MainActivity.kt
@@ -1,12 +1,17 @@
 package younippon.github.io.android_material_components
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().add(R.id.fragment_home, HomeFragment()).commit()
+        }
+
     }
+
 }
