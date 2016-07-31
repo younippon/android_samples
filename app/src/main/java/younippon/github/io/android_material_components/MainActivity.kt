594b636507154776ed61a65d83ee510b2ab6768c
younippon <hikonyan.mh36@gmail.com>
2016-07-31 16:26:10 +0900

    Added drawer

--- app/src/main/java/younippon/github/io/android_material_components/MainActivity.kt
+++ app/src/main/java/younippon/github/io/android_material_components/MainActivity.kt
@@ -1,17 +1,47 @@
 package younippon.github.io.android_material_components
 
 import android.os.Bundle
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        initToolbar(toolbar)
+        initDrawer(toolbar)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction().add(R.id.fragment_home, HomeFragment()).commit()
         }
-
     }
+
+    private fun initToolbar(toolbar: Toolbar) {
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+    }
+
+    private fun initDrawer(toolbar: Toolbar) {
+        val drawerLayout = findViewById(R.id.drawer) as DrawerLayout
+        val actionBarDrawerToggle = object : ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.openDrawer, R.string.closeDrawer) {
+
+            override fun onDrawerClosed(drawerView: View) {
+                super.onDrawerClosed(drawerView)
+            }
+            override fun onDrawerOpened(drawerView: View) {
+                super.onDrawerOpened(drawerView)
+            }
+        }
+        drawerLayout.setDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+    }
+
 
 }
